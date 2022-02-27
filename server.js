3bfcbcfaeaf0991071f1b9373b791feb770d1d8e
Madyson Barber <madysonb@live.unc.edu>
2022-02-26 21:14:12 -0500

    passing first 2 curls

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 // Import modules
-import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs"
+// import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs"
 
 // Require Express.js
 const express = require('express')
 const app = express()
 
 HTTP_PORT = 5000
-
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
-});
-
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+    console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
 });
 
 // Define default endpoint
 app.get('/app/', (req, res) => {
     // Respond with status 200
-        res.statusCode = 200;
+    res.statusCode = 200;
     // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage)
+});
+
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404).send('404 NOT FOUND')
 });
 
 
@@ -32,10 +31,125 @@ app.get('/app/', (req, res) => {
 app.get('/app/flips/:number', (req, res) => {
     const flips = coinFlips(req.params.number)
     const count = countFlips(flips)
-    res.status(200).json({"raw":flips,"summary":count})
+    res.status(200).json({ "raw": flips, "summary": count })
 });
 
-app.get('/app/flips/:number', (req, res) => {
-	const flips = coinFlips(req.params.number)
-	res.status(200).json(game)
+app.get('/app/flip/call/:guess(heads|tails)', (req, res) => {
+    const game = flipACoin(req.params.guess)
+    res.status(200).json(game)
 });
+
+
+
+// coin functions
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+function coinFlip() {
+    return Math.random() > .5 ? "heads" : "tails"
+}
+
+
+/** Multiple coin flips
+ * 
+ * Write a function that accepts one parameter (number of flips) and returns an array of 
+ * resulting "heads" or "tails".
+ * 
+ * @param {number} flips 
+ * @returns {string[]} results
+ * 
+ * example: coinFlips(10)
+ * returns:
+ *  [
+'heads', 'heads',
+'heads', 'tails',
+'heads', 'tails',
+'tails', 'heads',
+'tails', 'heads'
+]
+*/
+
+function coinFlips(flips) {
+    let results = []
+    let f = 0
+    while (f < flips) {
+        results[f] = coinFlip()
+        f = f + 1
+    }
+    return results
+}
+
+
+/** Count multiple flips
+ * 
+ * Write a function that accepts an array consisting of "heads" or "tails" 
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ * an object containing the number of each.
+ * 
+ * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+ * { tails: 5, heads: 5 }
+ * 
+ * @param {string[]} array 
+ * @returns {{ heads: number, tails: number }}
+ */
+
+function countFlips(array) {
+    let hCount = 0
+    let tCount = 0
+    for (let f in array) {
+        if (array[f] === 'heads') {
+            hCount += 1
+        } else {
+            tCount += 1
+        }
+    }
+
+    if (tCount == 0) {
+        return { 'heads': hCount }
+    }
+
+    if (hCount == 0) {
+        return { 'tails': tCount }
+    }
+
+    return { 'heads': hCount, 'tails': tCount }
+
+}
+
+
+/** Flip a coin!
+ * 
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+ * 
+ * @param {string} call 
+ * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+ * 
+ * example: flipACoin('tails')
+ * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+ */
+
+function flipACoin(call) {
+    let flip = coinFlip()
+    let result = ""
+    if (flip === call) {
+        result = 'win'
+    } else {
+        result = 'lose'
+    }
+
+    return { 'call': call, 'flip': flip, 'result': result }
+
+}
