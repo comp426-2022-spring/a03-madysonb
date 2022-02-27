189d529a52045205991dc755d0a97ee52193833b
Madyson Barber <madysonb@live.unc.edu>
2022-02-26 21:27:34 -0500

    remove hard coding port

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const app = express()
 
-HTTP_PORT = 5000
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
