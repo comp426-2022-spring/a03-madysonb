4ba9bb9bb3d0f7b163a2f7b3e86926daa65215f5
Madyson Barber <madysonb@live.unc.edu>
2022-02-26 20:23:11 -0500

    default endpoint

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ HTTP_PORT = 5000
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
